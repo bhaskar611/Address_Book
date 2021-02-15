0688ff7ce287bcb7a8f5ecba71673251386aef37
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-15 20:51:43 +0530

    Added:Welcome message and created contact details

---
+++ Address_Book/src/com/addressbook/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.addressbook;
+
+
+import java.util.*;
+class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address Book  System  ");
+    }
+
+}
