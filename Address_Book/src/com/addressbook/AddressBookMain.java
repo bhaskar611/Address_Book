9db8caeeaf1673212630dd578b2916f0bfddc4db
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-15 21:12:12 +0530

    Added:Edited contact details of person

--- Address_Book/src/com/addressbook/AddressBookMain.java
+++ Address_Book/src/com/addressbook/AddressBookMain.java
@@ -13,23 +13,41 @@ class AddressBookMain {
         System.out.println("Welcome to the Address Book  System  ");
        
 		AddressBook addressBook = new AddressBook();
-		System.out.println("Enter the contact details:");
-		String firstName = sc.next();
-		String lastName = sc.next();
-		String address = sc.next();
-		String city = sc.next();
-		String state = sc.next();
-		String email = sc.next();
-		String phoneNumber = sc.next();
-		String zip = sc.next();
-		ContactDetails cd = new ContactDetails(firstName, lastName, address, city, state, email, phoneNumber, zip);
+		boolean flag = true;
 
-		ArrayList<ContactDetails> contactList = addressBook.contactList(cd);
-		for(ContactDetails contact:contactList){
-			System.out.println(contact.getFirstName()+" "+contact.getLastName()+" "+contact.getAddress()+" "+contact.getCity()+
-					" "+contact.getState()+" "+contact.getEmail()+" "+contact.getPhoneNumber()+" "+contact.getZip());
-			
+		while(flag) {
+
+			System.out.println("1.Add Contact");
+			System.out.println("2.Edit Contact");
+			System.out.println("3.Exit");
+			System.out.println("Enter Choice: ");
+
+			int option = sc.nextInt();
+
+			switch (option)
+			{
+			case 1: 
+				addressBook.addContactDetails();
+				break;
+
+			case 2: 
+				System.out.println("Enter the Person First name to edit details: ");
+				String person_name = sc.next();
+
+				boolean b = addressBook.editContactDetails(person_name);
+				if (b == true) {
+					System.out.println("Details Updated");
+				} else {
+					System.out.println("Contact Not Found");
+				}
+				break;
+
+			case 3: 
+				flag =false;
+				break;
+
+			}
 		}
-    }
 
+	}
 }
